33ab9bd5fcbfad4074d24abe69f66c67845f5646
mgunnett <m-gunnett@onu.edu>
2024-12-11 22:28:47 -0500

    Add project files.

---
+++ Lab13PartB/Lab13B-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+bool isLeapYear(int year);
+
+int main() {
+    char choice;
+    int year;
+
+    cout << "Enter a year: ";
+    while (true) {
+        cin >> choice;
+        if (toupper(choice) == 'Q') {
+            break;
+        }
+        cin.unget(); // Put the character back into the input stream
+        cin >> year;
+
+        if (isLeapYear(year)) {
+            cout << year << " is a leap year.\n" << endl;
+        }
+        else {
+            cout << year << " is not a leap year.\n" << endl;
+        }
+
+        cout << "Enter a year or press 'Q' to quit: ";
+    }
+
+    return 0;
+}
+
+bool isLeapYear(int year) {
+    return (year >= 1582) && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+
