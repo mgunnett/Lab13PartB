22035ed91e1154d414682353cf937c9ccb291c43
mgunnett <m-gunnett@onu.edu>
2024-12-11 22:39:57 -0500

    Completed 13B-2

--- Lab13PartB/Lab13B-1.cpp
+++ Lab13PartB/Lab13B-1.cpp
@@ -1,38 +1,86 @@
 #include <iostream>
 #include <cctype>
+#include <string>
 
 using namespace std;
 
-bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 
 int main() {
     char choice;
-    int year;
+    int month, year;
+    string monthName;
+    
 
-    cout << "Enter a year: ";
+    cout << "Enter a month and a year: ";
     while (true) {
         cin >> choice;
         if (toupper(choice) == 'Q') {
-            break; 
+            break;
         }
         cin.unget(); // Put the character back into the input stream
-        cin >> year;
+        cin >> month >> year;
 
-        if (isLeapYear(year)) {
-            cout << year << " is a leap year.\n" << endl;
+        if (month == 1) {
+            monthName = "January";
         }
-        else {
-            cout << year << " is not a leap year.\n" << endl;
+        if (month == 2) {
+            monthName = "Feburary";
+        }
+        if (month == 3) {
+            monthName = "March";
+        }
+        if (month == 4) {
+            monthName = "April";
+        }
+        if (month == 5) {
+            monthName = "May";
+        }
+        if (month == 6) {
+            monthName = "June";
+        }
+        if (month == 7) {
+            monthName = "July";
+        }
+        if (month == 8) {
+            monthName = "August";
+        }
+        if (month == 9) {
+            monthName = "September";
+        }
+        if (month == 10) {
+            monthName = "October";
+        }
+        if (month == 11) {
+            monthName = "November";
+        }
+        if (month == 3) {
+            monthName = "December";
         }
 
-        cout << "Enter a year or press 'Q' to quit: ";
+        int days = daysInMonth(month, year);
+        cout << monthName << " " << year << " has " << days << " days.\n" << endl;
+
+        cout << "Enter a month and a year or press 'Q' to quit: ";
     }
 
     return 0;
 }
-
-bool isLeapYear(int year) {
-    return (year >= 1582) && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+/**
+ * daysInMonth – determines the number of days in a specified month
+ * @param month the month; expected to be in the range [1..12]
+ * @param year the year; expected to be >= 1582
+ * @return either 28, 29, 30, or 31, based on month and (leap) year
+ */
+int daysInMonth(int month, int year) {
+    if (month == 2) {
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return 30;
+    }
+    else {
+        return 31;
+    }
 }
-
 
